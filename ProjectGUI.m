e483990ad07c9ea13deb2a72a16f04fb958829c3
David-Steketee <david.stek@outlook.com>
2020-05-01 22:52:22 +0800

    Add empty GUI
    I made a basic guide with no functionality but some basic guide setup like scaleable window size and with button and axes layout

---
+++ ProjectGUI.m
@@ -0,0 +1,133 @@
+function varargout = ProjectGUI(varargin)
+% PROJECTGUI MATLAB code for ProjectGUI.fig
+%      PROJECTGUI, by itself, creates a new PROJECTGUI or raises the existing
+%      singleton*.
+%
+%      H = PROJECTGUI returns the handle to a new PROJECTGUI or the handle to
+%      the existing singleton*.
+%
+%      PROJECTGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PROJECTGUI.M with the given input arguments.
+%
+%      PROJECTGUI('Property','Value',...) creates a new PROJECTGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ProjectGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ProjectGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ProjectGUI
+
+% Last Modified by GUIDE v2.5 01-May-2020 22:48:56
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ProjectGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @ProjectGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ProjectGUI is made visible.
+function ProjectGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ProjectGUI (see VARARGIN)
+
+% Choose default command line output for ProjectGUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ProjectGUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ProjectGUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in load_trained_model.
+function load_trained_model_Callback(hObject, eventdata, handles)
+% hObject    handle to load_trained_model (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in train_new_model.
+function train_new_model_Callback(hObject, eventdata, handles)
+% hObject    handle to train_new_model (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in SaveModel.
+function SaveModel_Callback(hObject, eventdata, handles)
+% hObject    handle to SaveModel (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in load_other_model.
+function load_other_model_Callback(hObject, eventdata, handles)
+% hObject    handle to load_other_model (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in load_scene.
+function load_scene_Callback(hObject, eventdata, handles)
+% hObject    handle to load_scene (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in find_objects.
+function find_objects_Callback(hObject, eventdata, handles)
+% hObject    handle to find_objects (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in lines_toggle.
+function lines_toggle_Callback(hObject, eventdata, handles)
+% hObject    handle to lines_toggle (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of lines_toggle
+
+
+% --- Executes on button press in checkbox2.
+function checkbox2_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox2
