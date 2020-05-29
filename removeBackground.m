5d9a87c3b6562a7cbdfc161922c90c9c55e284da
Tom Almeida <tommoa256@gmail.com>
2020-05-29 14:12:40 +0800

    Add some comments to the code

--- removeBackground.m
+++ removeBackground.m
@@ -6,17 +6,36 @@ function removedMask = removeBackground(grayImage)
         error("removeBackground: grayImage isn't gray scale")
     end
 
+    % You might be wondering... why use a straight binary filter
+    % instead of going for edges?
+    % We found that doing edge detection was often unreliable, as the
+    % background surfaces - while generally of a uniform colour - had some
+    % minor imperfections that would be found as being lines.
+    % By using a binary filter, we had much better reliability with getting
+    % the area around an object as opposed to random lines that may not be
+    % related to the object.
+
+    % Do a gaussian filter to make the image smoothed. This helps get rid of
+    % some of the imperfections in the background.
     gaussian = imgaussfilt(grayImage, rows*columns/200000);
+    % We can determine if the background is white or black by comparing the
+    % mean of the grayed image with the median
     whiteBackground = (quantile(grayImage, 0.5, 'all') > mean(grayImage, 'all'));
     filtered = imbinarize(gaussian, graythresh(gaussian));
+    % If we have a white background, we need to flip the binarized image for
+    % our filter
     if whiteBackground
         filtered = ~filtered;
     end
+    % Fill in the holes in the image
     filtered = imfill(filtered, 'holes');
+    % Erode so that points not a part of the main object are removed
     se = strel('disk', 10);
     filtered = imerode(filtered, se);
+    % Fill in the holes
     binary = imfill(filtered, 'holes');
 
+    % Do a convex hull so we cover the entire object
     convexHull = bwconvhull(binary);
 
 removedMask = convexHull;
--- show_matched_masks.m
+++ show_matched_masks.m
@@ -3,6 +3,7 @@ function show_matched_masks(scene_image, object_images, masks, transforms, scale
 
     num_objs = length(object_images);
     each_obj_h = size(scene_image,1)/num_objs;
+    % For each object found in the scene...
     for ii = 1:num_objs
         colour = colours(1 + mod(ii-1, length(colours)));
         I1 = scene_image;
@@ -14,21 +15,26 @@ function show_matched_masks(scene_image, object_images, masks, transforms, scale
         scale = scales{ii};
         scale2 = size(I2,1)/size(I1,1)*num_objs;
 
-        current_mask = bwboundaries(masks{ii});
-        current_mask = current_mask{1};
-        current_mask = fliplr(current_mask);
-        % Test masks
+        % Masks on the main image
         test_mask = bwboundaries(masks{ii});
         test_mask = fliplr(test_mask{1});
+        % Get correct rotation and location
         test_mask = transformPointsForward(transforms{ii}, test_mask);
+        % Move to correct offset
         test_mask = bsxfun(@plus, test_mask, offset1);
         p = patch(test_mask(:, 1), test_mask(:, 2), colour);
         p.FaceVertexAlphaData = 0.3;
         p.FaceAlpha = 'flat';
 
-        % For the training images
+        % For the training images on the right hand side
+        current_mask = bwboundaries(masks{ii});
+        current_mask = current_mask{1};
+        current_mask = fliplr(current_mask);
+        % Unscale mask to full size
         current_mask = bsxfun(@rdivide, current_mask, scale);
+        % Scale to size of side image
         current_mask = bsxfun(@rdivide, current_mask, scale2);
+        % Move to correct position
         current_mask = bsxfun(@plus, current_mask, offset2);
         p = patch(current_mask(:, 1), current_mask(:, 2), colour);
         p.FaceVertexAlphaData = 0.3;
